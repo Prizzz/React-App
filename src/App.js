c87bdb83f67bf79c444c77360393a77298cf2d68
Prizzz <rob_243248@mail.ru>
2022-08-29 19:02:46 +0400

    feat: add get data from api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./styles/App.css";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
@@ -6,17 +6,22 @@ import PostFilter from "./components/PostFilter";
 import { usePosts } from "./components/hooks/usePosts";
 import MyModal from "./components/UI/modal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
+import PostService from "./components/API/PostServie";
 
 function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: "JavaScript", body: "Web" },
-    { id: 2, title: "Python", body: "Django" },
-    { id: 3, title: "Go", body: "Back" },
-    { id: 4, title: "React", body: "Front" },
-  ]);
+  const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  async function fetchPosts() {
+    const posts = await PostService.getAll();
+    setPosts(posts);
+  }
 
   function createPost(newPost) {
     setPosts([...posts, newPost]);
